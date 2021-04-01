c888ea5cdb4f5422f8b15a38d0674afaba7d56d9
seenline <1078657808@qq.com>
2021-04-01 21:13:25 +0800

    feat: text和name变为参数

--- src/main/java/com/wizzstudio/push/Controller.java
+++ src/main/java/com/wizzstudio/push/Controller.java
@@ -1,16 +1,19 @@
 package com.wizzstudio.push;
 
+import com.google.common.base.Strings;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.cp.api.impl.WxCpServiceImpl;
 import me.chanjar.weixin.cp.bean.message.WxCpMessage;
 import me.chanjar.weixin.cp.config.impl.WxCpDefaultConfigImpl;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     @GetMapping("/push")
-    public String get() throws WxErrorException {
+
+    public String get(@RequestParam(required = true) String name, @RequestParam(required = false) String text) throws WxErrorException {
         WxCpDefaultConfigImpl config = new WxCpDefaultConfigImpl();
         config.setCorpId(Config.getCorpId());
         config.setCorpSecret(Config.getCorpSecret());
@@ -18,10 +21,7 @@ public class Controller {
 
         WxCpServiceImpl wxCpService = new WxCpServiceImpl();
         wxCpService.setWxCpConfigStorage(config);
-
-        String userId = "QiHaoTian";
-        WxCpMessage message = WxCpMessage.TEXT().agentId(1000002).toUser(userId).content("Hello World").build();
-
+        WxCpMessage message = WxCpMessage.TEXT().agentId(1000002).toUser(name).content(text).build();
         wxCpService.getMessageService().send(message);
         return "hello";
     }
