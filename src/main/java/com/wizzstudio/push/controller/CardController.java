483d0bf2bb86dec85b67534a847e8f43b8588104
117503445 <t117503445@gmail.com>
2021-04-08 20:34:00 +0800

    feat: CardController

---
+++ src/main/java/com/wizzstudio/push/controller/CardController.java
@@ -0,0 +1,38 @@
+package com.wizzstudio.push.controller;
+
+import com.wizzstudio.push.config.FileConfig;
+import com.wizzstudio.push.model.CommonResult;
+import com.wizzstudio.push.service.WechatService;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.cp.bean.message.WxCpMessage;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
+@RestController
+public class CardController {
+    @RequestMapping("/push/card/v1")
+    public CommonResult<String> get(HttpServletRequest request, @RequestParam(required = true) String name, @RequestParam(required = false) String text) throws WxErrorException, IOException {
+
+        String content;
+
+        String bodyText = new String(request.getInputStream().readAllBytes());
+        if (bodyText.length() > 0) {
+            content = bodyText;
+        } else if (text != null && text.length() > 0) {
+            content = text;
+        } else {
+            return new CommonResult<String>(0, "id not found", "");
+
+        }
+
+        WxCpMessage message = WxCpMessage.TEXTCARD().agentId(FileConfig.getAgentId()).toUser(name).title("Mytitle").description("myde").url("https://www.baidu.com").btnTxt("btn").build();
+        WechatService.getWxCpService().getMessageService().send(message);
+
+        return new CommonResult<String>(0, "success", "");
+    }
+}
