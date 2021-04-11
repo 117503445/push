3b100357511a8bdfd14831ee2ad9704567009c63
117503445 <t117503445@gmail.com>
2021-04-11 17:40:17 +0800

    feat: 捕获全局异常

--- src/main/java/com/wizzstudio/push/errordeal/BizException.java
+++ src/main/java/com/wizzstudio/push/errordeal/BizException.java
@@ -1,39 +1,39 @@
 package com.wizzstudio.push.errordeal;
 
-public class BizExpection extends RuntimeException {
+public class BizException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
     protected String errorCode;
     protected String errorMsg;
 
-    public BizExpection() {
+    public BizException() {
         super();
     }
 
-    public BizExpection(BaseErrorInfoInterface errorInfoInterface) {
+    public BizException(BaseErrorInfoInterface errorInfoInterface) {
         super(errorInfoInterface.getResultCode());
         this.errorCode = errorInfoInterface.getResultCode();
         this.errorMsg = errorInfoInterface.getResultMsg();
     }
 
-    public BizExpection(BaseErrorInfoInterface errorInfoInterface, Throwable cause) {
+    public BizException(BaseErrorInfoInterface errorInfoInterface, Throwable cause) {
         super(errorInfoInterface.getResultCode(), cause);
         this.errorCode = errorInfoInterface.getResultCode();
         this.errorMsg = errorInfoInterface.getResultMsg();
     }
 
-    public BizExpection(String errorMsg) {
+    public BizException(String errorMsg) {
         super(errorMsg);
         this.errorMsg = errorMsg;
     }
 
-    public BizExpection(String errorCode, String errorMsg) {
+    public BizException(String errorCode, String errorMsg) {
         super(errorCode);
         this.errorCode = errorCode;
         this.errorMsg = errorMsg;
     }
 
-    public BizExpection(String errorCode, String errorMsg, Throwable cause) {
+    public BizException(String errorCode, String errorMsg, Throwable cause) {
         super(errorCode, cause);
         this.errorCode = errorCode;
         this.errorMsg = errorMsg;
