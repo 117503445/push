85aa59c06e72422d1c59ff812fc447586925dd9f
117503445 <t117503445@gmail.com>
2021-04-17 02:42:15 +0800

    feat(global): HttpLog
    使用 AOP 对每个 HTTP 请求进行截取 打印
    
    todo
    1. 把日志发送到 ES 上去
    2. 不要在控制台输出日志
    3. 出现异常时，GlobalExceptionHandler 会截断 AOP 的流程

--- src/main/java/com/wizzstudio/push/config/GlobalExceptionHandler.java
+++ src/main/java/com/wizzstudio/push/config/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.wizzstudio.push.config;
 
 
 import com.wizzstudio.push.model.CommonResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * 全局异常处理
  */
-//@ControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
---
+++ src/main/java/com/wizzstudio/push/log/HttpLog.java
@@ -0,0 +1,102 @@
+package com.wizzstudio.push.log;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class HttpLog {
+    private final static Logger logger = LoggerFactory.getLogger(HttpLog.class);
+
+    /**
+     * 以 controller 包下定义的所有请求为切入点
+     */
+    @Pointcut("execution(public * com.wizzstudio.push.controller..*.*(..))")
+    public void webLog() {
+    }
+
+    /**
+     * 在切点之前织入
+     *
+     * @param joinPoint
+     * @throws Throwable
+     */
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) {
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            logger.info("========================================== Start ==========================================");
+            logger.info("{} {}", request.getMethod(), request.getRequestURI() + "?" + request.getQueryString());
+            // 打印调用 controller 的全路径以及执行方法
+            logger.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            // 打印请求的 IP
+            logger.info("IP             : {}", getIpAddr(request));
+        }
+    }
+
+
+    /**
+     * 环绕
+     *
+     * @param proceedingJoinPoint
+     * @return
+     * @throws Throwable
+     */
+    @Around("webLog()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            var response = attributes.getResponse();
+            if (response != null) {
+                logger.info("Status Code  : {}", response.getStatus());
+            }
+        }
+        logger.info("Response Args  : {}", result);
+        // 执行耗时
+        logger.info("Time-Consuming : {} ms", System.currentTimeMillis() - startTime);
+        logger.info("=========================================== End ===========================================");
+        logger.info("");
+        return result;
+    }
+
+    /**
+     * 在切点之后织入
+     *
+     * @throws Throwable
+     */
+    @After("webLog()")
+    public void doAfter() {
+
+    }
+
+    public String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+}
