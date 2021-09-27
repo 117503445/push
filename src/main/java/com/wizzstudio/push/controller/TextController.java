7f4da3054fd1baa813684e133ceae4c075aa362c
TheR1sing3un <1955938454@qq.com>
2021-09-28 00:04:54 +0800

    feat(wechat): 完成了v2文本推送接口
    1.根据昵称映射完成了TEXT文本的推送
    
    [skip ci]
    
    Signed-off-by: TheR1sing3un <1955938454@qq.com>

--- src/main/java/com/wizzstudio/push/controller/TextController.java
+++ src/main/java/com/wizzstudio/push/controller/TextController.java
@@ -4,9 +4,11 @@ import com.wizzstudio.push.config.ApiVersion;
 import com.wizzstudio.push.config.ESConfig;
 import com.wizzstudio.push.config.StaticFactory;
 import com.wizzstudio.push.model.CommonResult;
+import com.wizzstudio.push.service.UserService;
 import com.wizzstudio.push.service.WechatService;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.cp.bean.message.WxCpMessage;
+import me.chanjar.weixin.cp.bean.messagebuilder.TextBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,31 +24,43 @@ public class TextController {
     @Autowired
     ESConfig esConfig;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/{version}")
     @ApiVersion
     public CommonResult pushText(HttpServletRequest request, @RequestParam() String name, @RequestParam(required = false) String text) throws WxErrorException, IOException {
-        String content;
-        String bodyText = new String(request.getInputStream().readAllBytes());
-        if (bodyText.length() > 0) {
-            content = bodyText;
-        } else if (text != null && text.length() > 0) {
-            content = text;
-        } else {
-            return new CommonResult(2, "content not found", null);
-        }
+        //获取推送的正文
+        String content = getContent(request, text);
         WxCpMessage message = WxCpMessage.TEXT().agentId(StaticFactory.getWeChatConfig().getAgentId()).toUser(name).content(content).build();
         WechatService.getWxCpService().getMessageService().send(message);
         return CommonResult.ok();
     }
 
-
     @RequestMapping("/{version}")
     @ApiVersion(2)
     public CommonResult pushTextWithNickname(HttpServletRequest request, @RequestParam() String name, @RequestParam(required = false) String text) throws IOException, WxErrorException {
-        String bodyText = new String(request.getInputStream().readAllBytes());
-        System.out.println("bodyText:"+bodyText);
-        System.out.println("text:"+text);
+        //获取推送的正文
+        String content = getContent(request,text);
+        //组成回复正文
+        String replyContent = name+",您有一条新的推送:\n"+content;
+        //根据昵称去获取用户id
+        String userId = userService.getUserIdByAbleNickname(name);
+        //根据id发送消息
+        WxCpMessage message = WxCpMessage.TEXT().agentId(StaticFactory.getWeChatConfig().getAgentId()).toUser(userId).content(replyContent).build();
+        WechatService.getWxCpService().getMessageService().send(message);
         return CommonResult.ok();
+    }
+
+    private String getContent(HttpServletRequest request, String text) throws IOException {
+        String bodyText = new String(request.getInputStream().readAllBytes());
+        if (bodyText.length() > 0) {
+            return bodyText;
+        } else if (text != null && text.length() > 0) {
+            return text;
+        } else {
+            return null;
+        }
     }
 
 }
